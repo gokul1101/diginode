263f02d879f45e65f02ad7155d9718b0892e4366
Dhanush Karthick <dhanukarthick15@gmail.com>
2021-04-02 13:02:57 +0530

    add or create playlist

--- client/src/components/Main/Iframe/Iframe.jsx
+++ client/src/components/Main/Iframe/Iframe.jsx
@@ -2,7 +2,52 @@ import React, { useState } from "react";
 import "./Iframe.css";
 import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
 import { IconButton } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { makeStyles } from "@material-ui/core/styles";
+import FormatIndentDecreaseOutlinedIcon from "@material-ui/icons/FormatIndentDecreaseOutlined";
+import AddCircleOutlinedIcon from "@material-ui/icons/AddCircleOutlined";
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
 const Iframe = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      flexWrap: "wrap",
+      "& > *": {
+        margin: theme.spacing(0.5),
+      },
+    },
+  }));
+
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [createPlaylist, setCreatePlaylist] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClick = () => {
+    props.snackBar("Added to Playlist", "success");
+    setOpen(false);
+  };
+
+  const onCreatedList = () => {
+    props.snackBar("Successfully created Playlist", "success");
+    setOpen(false);
+  };
   const [checkFavorite, setCheckFavorite] = useState(
     props.favorites.filter((fav) => fav.videoId === props.currentVideo.videoId)
       .length === 1
@@ -99,7 +144,101 @@ const Iframe = (props) => {
               title="Add to Playlist"
               className="fas fa-sliders-h"
               style={{ fontSize: "26px", marginTop: "-8px" }}
+              onClick={handleClickOpen}
             ></i>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="responsive-dialog-title"
+            >
+              <DialogTitle id="responsive-dialog-title">
+                {"ADD OR CREATE PLAYLIST"}
+              </DialogTitle>
+              <DialogContent>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  endIcon={<AddCircleOutlinedIcon />}
+                  onClick={() => {
+                    setCreatePlaylist(false);
+                  }}
+                >
+                  ADD PLAYLIST
+                </Button>
+                <Button
+                  variant="outlined"
+                  className={classes.button}
+                  color="secondary"
+                  endIcon={<FormatIndentDecreaseOutlinedIcon />}
+                  onClick={() => {
+                    setCreatePlaylist(true);
+                  }}
+                >
+                  CREATE PLAYLIST
+                </Button>
+                <div className="li-overlay mt-3">
+                  {createPlaylist ? (
+                    <div className="d-flex">
+                      <input
+                        className="p-2 mr-3"
+                        placeholder="Name of the playlist"
+                        style={{
+                          border: "none",
+                          borderRadius: "10px",
+                        }}
+                      />
+                      <Button
+                        size="small"
+                        variant="contained"
+                        color="secondary"
+                        endIcon={<AddCircleOutlinedIcon />}
+                        onClick={onCreatedList}
+                      >
+                        ADD
+                      </Button>
+                    </div>
+                  ) : (
+                    <ul>
+                      <div
+                        className="scroll-li"
+                        style={{
+                          width: "100%",
+                          height: "100px",
+                          overflowY: "scroll",
+                        }}
+                      >
+                        <li className="text-left">
+                          <Chip
+                            avatar={<Avatar>D</Avatar>}
+                            label="Dhanush"
+                            clickable
+                            color="primary"
+                            onClick={handleClick}
+                            className="mb-2 "
+                          />
+                        </li>
+
+                        <li>
+                          <Chip
+                            avatar={<Avatar>G</Avatar>}
+                            label="Gokul"
+                            clickable
+                            color="primary"
+                            onClick={handleClick}
+                            className="mb-2"
+                          />
+                        </li>
+                      </div>
+                    </ul>
+                  )}
+                </div>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary" autoFocus>
+                  CLOSE
+                </Button>
+              </DialogActions>
+            </Dialog>
           </div>
         </div>
       </div>
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -2,40 +2,12 @@ import React, { useState } from "react";
 import Navbar from "./Navbar/Navbar";
 import "./Main.css";
 import Iframe from "./Iframe/Iframe";
-// import { Button, colors, TextField } from "@material-ui/core";
-// import { makeStyles } from "@material-ui/core/styles";
-// import FormatIndentDecreaseOutlinedIcon from "@material-ui/icons/FormatIndentDecreaseOutlined";
-// import AddCircleOutlinedIcon from "@material-ui/icons/AddCircleOutlined";
-// import Avatar from "@material-ui/core/Avatar";
-// import Chip from "@material-ui/core/Chip";
-// import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
-// import { IconButton } from "@material-ui/core";
 const Main = (props) => {
-  // const useStyles = makeStyles((theme) => ({
-  //   button: {
-  //     margin: theme.spacing(1),
-  //     border: "1px solid cyan",
-  //     color: "cyan",
-  //   },
-  //   root: {
-  //     display: "flex",
-  //     justifyContent: "center",
-  //     flexWrap: "wrap",
-  //     "& > *": {
-  //       margin: theme.spacing(0.5),
-  //     },
-  //   },
-  // }));
-  // const classes = useStyles();
   const [favorites, setFavorites] = useState(
     Object.keys(props.user).length === 0 ? [] : props.user.favorites
   );
   const [toggle, setToggle] = useState(false);
   const [currentVideo, setCurrentVideo] = useState([]);
-  const [createPlaylist, setCreatePlaylist] = useState(false);
-  const handleClick = () => {
-    alert("clicked");
-  };
   return (
     <div className="main h-100">
       <Navbar
@@ -49,89 +21,12 @@ const Main = (props) => {
         setCurrentVideo={setCurrentVideo}
       />
       {toggle ? (
-        // <div className="playlist-overlay d-flex flex-column align-items-center">
-        //   <IconButton
-        //     className="close-btn position-absolute"
-        //     aria-label="delete"
-        //     style={{ color: "white" }}
-        //   >
-        //     <CloseOutlinedIcon />
-        //   </IconButton>
-        //   <div className="d-flex align-items-center justify-content-center mt-3">
-        //     <Button
-        //       variant="outlined"
-        //       className={classes.button}
-        //       endIcon={<AddCircleOutlinedIcon />}
-        //       onClick={() => {
-        //         setCreatePlaylist(false);
-        //       }}
-        //     >
-        //       ADD PLAYLIST
-        //     </Button>
-        //     <Button
-        //       variant="outlined"
-        //       className={classes.button}
-        //       endIcon={<FormatIndentDecreaseOutlinedIcon />}
-        //       onClick={() => {
-        //         setCreatePlaylist(true);
-        //       }}
-        //     >
-        //       CREATE PLAYLIST
-        //     </Button>
-        //   </div>
-        //   <div className="li-overlay mt-3">
-        //     {createPlaylist ? (
-        //       <div className="d-flex">
-        //         <input
-        //           className="p-2 mr-3"
-        //           placeholder="Name of the playlist"
-        //           style={{
-        //             color: "white",
-        //             background: "rgba(255, 255, 255, 0.09)",
-        //             border: "none",
-        //             borderRadius: "10px",
-        //           }}
-        //         />
-        //         <Button
-        //           size="small"
-        //           variant="contained"
-        //           color="secondary"
-        //           endIcon={<AddCircleOutlinedIcon />}
-        //         >
-        //           ADD
-        //         </Button>
-        //       </div>
-        //     ) : (
-        //       <ul>
-        //         <li>
-        //           <Chip
-        //             avatar={<Avatar>D</Avatar>}
-        //             label="Dhanush"
-        //             clickable
-        //             color="primary"
-        //             onClick={handleClick}
-        //             className="mb-2"
-        //           />
-        //         </li>
-        //         <li>
-        //           <Chip
-        //             avatar={<Avatar>G</Avatar>}
-        //             label="Gokul"
-        //             clickable
-        //             color="primary"
-        //             onClick={handleClick}
-        //             className="mb-2"
-        //           />
-        //         </li>
-        //       </ul>
-        //     )}
-        //   </div>
-        // </div>
         <Iframe
           setToggle={setToggle}
           favorites={favorites}
           setFavorites={setFavorites}
           currentVideo={currentVideo}
+          snackBar={props.snackBar}
         />
       ) : null}
     </div>
