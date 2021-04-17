e264f8a3be7d662f1c726534625253432646f262
Gokul <gokuls2781@gmail.com>
2021-04-17 16:29:37 +0530

    playlist design

--- client/src/components/Main/Navbar/Playlist/Playlist.jsx
+++ client/src/components/Main/Navbar/Playlist/Playlist.jsx
@@ -4,15 +4,21 @@ import React, { useState } from "react";
 import "./Playlist.css";
 
 const Playlist = (props) => {
-  let [playlists, setPlaylists] = useState(props.playlists? props.playlists : []);
-  let [searchPlaylists, setSearchPlaylists] = useState(props.playlists? props.playlists : []);
+  let [playlists, setPlaylists] = useState(
+    props.playlists ? props.playlists : []
+  );
+  let [searchPlaylists, setSearchPlaylists] = useState(
+    props.playlists ? props.playlists : []
+  );
   const search = (name) => {
-    if(name === "") setSearchPlaylists(playlists);
+    if (name === "") setSearchPlaylists(playlists);
     else {
-      let arr = playlists.filter(playlist => playlist.name.toLowerCase().includes(name.toLowerCase()));
-      setSearchPlaylists(arr)
+      let arr = playlists.filter((playlist) =>
+        playlist.name.toLowerCase().includes(name.toLowerCase())
+      );
+      setSearchPlaylists(arr);
     }
-  }
+  };
   return (
     <div className="container-fluid playlists d-flex">
       <div className="px-2 m-4">
@@ -33,46 +39,51 @@ const Playlist = (props) => {
           </div>
         </form>
         <div className="d-flex align-items-center justify-content-center flex-wrap mt-5 pl-3 h-100">
-          {searchPlaylists.map((playlist) => {
-              return (
-                <div className="flex-fill playlist-card m-3">
-                  <div className="playlist h-100 w-100">
-                    <div className="playlist-img position-relative">
-                      {playlist.list.length !== 0 ? (
-                        <>
-                          <img
-                            className="img-fluid"
-                            src={playlist.list[0].thumbnails}
-                            alt="playlist-img"
-                          />
-                          <div className="playlist-play-icon d-flex align-items-center justify-content-center position-absolute">
-                            <IconButton aria-label="add an alarm">
-                              <PlayArrowIcon
-                                style={{
-                                  color: "#f55050",
-                                  height: 40,
-                                  width: 40,
-                                }}
-                              />
-                            </IconButton>
-                          </div>
-                        </>
-                      ) : (
-                        <></>
-                      )}
-                    </div>
-                    <div className="playlist-detail d-flex align-items-center justify-content-between mt-2 pt-1 px-4">
-                      <h6 className="text-white m-0">{playlist.name}</h6>
-                      <p className="m-0 text-white">
-                        {playlist.list.length !== 1
-                          ? `${playlist.list.length} videos`
-                          : `${playlist.list.length} video`}
-                      </p>
-                    </div>
+          {searchPlaylists.map((playlist, index) => {
+            return (
+              <div
+                className="flex-fill playlist-card m-3"
+                onClick={(e) => console.log(e.currentTarget.id)}
+                key={index}
+                id={playlist? playlist.name : index}
+              >
+                <div className="playlist h-100 w-100">
+                  <div className="playlist-img position-relative">
+                    {playlist.list.length !== 0 ? (
+                      <>
+                        <img
+                          className="img-fluid"
+                          src={playlist.list[0].thumbnails}
+                          alt="playlist-img"
+                        />
+                        <div className="playlist-play-icon d-flex align-items-center justify-content-center position-absolute">
+                          <IconButton aria-label="add an alarm">
+                            <PlayArrowIcon
+                              style={{
+                                color: "#f55050",
+                                height: 40,
+                                width: 40,
+                              }}
+                            />
+                          </IconButton>
+                        </div>
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                  <div className="playlist-detail d-flex align-items-center justify-content-between mt-2 pt-1 px-4">
+                    <h6 className="text-white m-0">{playlist.name}</h6>
+                    <p className="m-0 text-white">
+                      {playlist.list.length !== 1
+                        ? `${playlist.list.length} videos`
+                        : `${playlist.list.length} video`}
+                    </p>
                   </div>
                 </div>
-              );
-            })}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
